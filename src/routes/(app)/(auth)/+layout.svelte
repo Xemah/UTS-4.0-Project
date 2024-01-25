6b9e7cc6bedd55be99556655ecf75041e5ccb79a
Xemah <admin@xemah.com>
2024-01-25 20:03:57 +0500

    Update

--- src/routes/(app)/(auth)/+layout.svelte
+++ src/routes/(app)/(auth)/+layout.svelte
@@ -8,7 +8,7 @@
 		class="flex-grow bg-center bg-cover bg-no-repeat -lg:hidden"
 		style="background-image: url('{authCover}')"
 	/>
-	<div class="flex flex-col items-center justify-center w-full max-w-[50vw] min-w-[500px] mx-auto">
+	<div class="flex flex-col items-center justify-center w-full md:max-w-[50vw] md:min-w-[500px] mx-auto">
 		<Container class="max-w-[400px]">
 			<slot />
 		</Container>
